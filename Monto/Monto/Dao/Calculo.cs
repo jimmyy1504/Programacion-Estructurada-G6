b3dfcbe49106c2d87d842bf7e7574121dd1e8d4d
Jimmy Selva <jimmyselvag@uamv.edu.ni>
2025-09-22 01:12:01 -0600

    Add files via upload

---
+++ Monto/Monto/Dao/Calculo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monto.Dao
+{
+    public static class Calculo
+    {
+        /// <summary>
+        /// Calcular monto por paso de valor
+        /// </summary>
+        /// <param name="precio"></param>
+        /// <returns></returns>
+        public static double CalcularMonto(Double precio)
+        {
+            precio *= 1.15;
+            return precio;
+        }
+
+        /// <summary>
+        /// Calcular monto por paso de referencia
+        /// </summary>
+        /// <param name="precio"></param>
+        /// <returns></returns>
+        public static double CalcularMonto(ref Double precio)
+        {
+            precio *= 1.15;
+            return precio;
+        }
+    }
+}
---
+++ Monto/Monto/Form1.cs
@@ -0,0 +1,40 @@
+﻿using Monto.Dao;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblMonto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double precio = double.Parse(tbPrecio.Text);
+            lblMonto.Text = " Precio: " + precio + "Monto: " +  
+                Calculo.CalcularMonto(precio) + "Precio: " + precio;
+        }
+
+        private void btnCalcularRef_Click(object sender, EventArgs e)
+        {
+            double precio = double.Parse(tbPrecio.Text);
+            lblMonto.Text = " Precio: " + precio+ "Monto: " + 
+                Calculo.CalcularMonto(ref precio) + "Precio: " + precio;
+        }
+    }
+}
