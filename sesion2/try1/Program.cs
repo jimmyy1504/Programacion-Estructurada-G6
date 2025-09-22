e6d086a58c3b6ffca55faecf73216603c6f8c6d6
Jimmy Selva <jimmyselvag@uamv.edu.ni>
2025-09-21 22:53:37 -0600

    Add files via upload

---
+++ sesion2/try1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace try1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Exception ex;
+                Console.Write("UAM");
+                Console.WriteLine(" - Managua");
+                int num;
+                Console.WriteLine("Dime tu edad:");
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
---
+++ sesion2/try2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Design;
+
+namespace try2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introducir edad: ");
+            int edad;
+            edad = int.Parse(Console.ReadLine());
+            if (edad > 14)
+            {
+                Console.WriteLine("Eres mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de edad");
+            }
+         }
+
+    }
+}
